0523ecb02a86ed9561e94634d24a98600dfaa1a3
Nick Dexter <nickdexter23@gmail.com>
2022-05-27 18:38:58 -0500

    commit tree abc8b65cedcd44058cdf66825bd9688a50d97c4b

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+document.querySelector("main#main").remove()
+const newHeader = document.createElement("h1")
+newHeader.innerHTML =
+  "<h1>Poodles!</h1>";
+document.getElementById('victory').innerHTML
